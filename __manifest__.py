0bb258d312a21cceab2fd9e6cd1ebc9d2871c96f
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-07-29 00:11:40 -0600

    Up

--- __manifest__.py
+++ __manifest__.py
@@ -1,11 +1,15 @@
-# __manifest__.py
 {
     'name': 'Product Custom Fields Extension',
-    'version': '1.0',
+    'version': '1.3',
     'category': 'Product',
     'summary': 'Extiende el modelo de producto para incluir campos personalizados adicionales',
     'author': 'Alphaqueb Consulting S.A.S.',
     'depends': ['product'],
+    'assets': {
+        'web.assets_backend': [
+            'product_custom_fields_extension/static/src/js/product_template_visibility.js',
+        ],
+    },
     'installable': True,
     'application': False,
 }
---
+++ static/src/js/product_template_visibility.js
@@ -0,0 +1,30 @@
+/** @odoo-module **/
+
+import { registry } from '@web/core/registry';
+import { Component, onMounted, useState } from '@odoo/owl';
+
+class ProductTemplateVisibility extends Component {
+    setup() {
+        this.state = useState({
+            isPaperRoll: this.props.record.data.is_paper_roll,
+        });
+
+        onMounted(() => {
+            this.env.bus.on('FIELD_CHANGE', this, this.onFieldChange);
+        });
+    }
+
+    willUnmount() {
+        this.env.bus.off('FIELD_CHANGE', this, this.onFieldChange);
+    }
+
+    onFieldChange({ dataPointId, changes }) {
+        if (dataPointId === this.props.record.id && 'is_paper_roll' in changes) {
+            this.state.isPaperRoll = changes.is_paper_roll;
+        }
+    }
+}
+
+ProductTemplateVisibility.template = 'product.ProductTemplateVisibility';
+
+registry.category('fields').add('product_template_visibility', ProductTemplateVisibility);
